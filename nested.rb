97d2318442327aebb55310983480cf2bffbad58e
Kathy B <kathybuggs@gmail.com>
2019-09-02 18:46:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,8 +80,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[0] = {:yukihiro_matsumoto => {:known_for => "Ruby", :languages =>["LISP", "C"]}}
-programmer_hash
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages =>["LISP", "C"]}
+    
 end
 
 def changing_alan
